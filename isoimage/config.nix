d4b2bf7fc20b4c7ae0c213dd6bb657f4010e7086
pagedmov <kylerclay@proton.me>
2025-08-11 14:08:29 -0400

    Included a custom /etc/issue with the installer iso

--- isoimage/config.nix
+++ isoimage/config.nix
@@ -3,6 +3,22 @@
     "${modulesPath}/installer/cd-dvd/installation-cd-minimal.nix"
   ];
 
+  environment.etc."issue".text = ''
+\e[92m<<< Welcome to NixOS 25.11.20250728.dc96378 \r (\m) - \l >>>\e[0m
+The "nixos" and "root" accounts have empty passwords.
+
+To log in over ssh you must set a password for either "nixos" or "root"
+with `passwd` (prefix with `sudo` for "root"), or add your public key to
+/home/nixos/.ssh/authorized_keys or /root/.ssh/authorized_keys.
+
+To set up a wireless connection, run `nmtui`.
+
+
+Run 'sudo nixos-wizard' to enter the installer.
+
+Run 'nixos-help' for the NixOS manual.
+  '';
+
   environment.systemPackages = [
     pkgs.nixfmt
     pkgs.nixfmt-classic
